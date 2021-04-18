f534d87bf9fad38b230c2b5044a11920895be49f
Sevag Hanssian <sevagh@protonmail.com>
2021-04-18 00:03:08 -0400

    Prepare boxplot script

--- mss_evaluation/sigsep-mus-oracle/thesis_validation.sh
+++ mss_evaluation/sigsep-mus-oracle/thesis_validation.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IRM.py --alpha=1 --eval_dir ./IRM1/ --max-tracks=1
-MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IRM.py --alpha=2 --eval_dir ./IRM2/ --max-tracks=1
+rm sisecc.pandas
+rm -rf ./experiment-02
 
-MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IRM_CQT.py --alpha=1 --eval_dir ./IRM1-CQT/  --max-tracks=1
-MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IRM_CQT.py --alpha=2 --eval_dir ./IRM2-CQT/  --max-tracks=1
+MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IRM.py --alpha=1 --eval_dir ./experiment-02/IRM1/
+MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IRM.py --alpha=2 --eval_dir ./experiment-02/IRM2/
+MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IBM.py --alpha=2 --eval_dir ./experiment-02/IBM2/
+MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IBM.py --alpha=1 --eval_dir ./experiment-02/IBM2/
+
+MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IRM_CQT.py --alpha=1 --eval_dir ./experiment-02/IRM1-CQT/
+MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IRM_CQT.py --alpha=2 --eval_dir ./experiment-02/IRM2-CQT/
+MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IBM_CQT.py --alpha=1 --eval_dir ./experiment-02/IBM1-CQT/
+MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ python IBM_CQT.py --alpha=2 --eval_dir ./experiment-02/IBM2-CQT/
+
+python aggregate.py experiment-02/* --out=sisecc.pandas
+python boxplot.py ./sisecc.pandas
