67a355263976ff6df0a826de845fcddd0844d9f0
Sevag Hanssian <sevagh@protonmail.com>
2021-04-25 12:05:40 -0400

    Rename IRM -> ideal_mask

--- mss_evaluation/oracle-cqt/thesis_validation.sh
+++ mss_evaluation/oracle-cqt/thesis_validation.sh
@@ -8,12 +8,7 @@ rm -rf ${exp_dir}
 export MUSDB_MAX_TRACKS=1
 export MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ
 
-# cqt starting from 27.5hz, 96 bins per octave
-echo "debug cq-nsgt perf"
-pybin="/home/sevagh/venvs/nsgt-accel/bin/python3"
-time $pybin evaloracle/IRM.py --eval_dir ./${exp_dir}/ ./config.json
-
-exit 0
+time $pybin evaloracle/ideal_mask.py --eval_dir ./${exp_dir}/ ./config.json
 
 $pybin evaloracle/aggregate.py ./${exp_dir}/* --out=${exp_dir}/sisecc.pandas
 $pybin evaloracle/boxplot.py ./${exp_dir}/sisecc.pandas ./${exp_dir}/oracle_boxplot.pdf
