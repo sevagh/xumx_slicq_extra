896bc311de5ceb8b522e5f8d32739ce9eb739ed1
Sevag Hanssian <sevagh@protonmail.com>
2021-07-31 08:04:42 -0400

    Some cleanups

--- mss_evaluation/04-umx-models.sh
+++ mss_evaluation/04-umx-models.sh
@@ -10,6 +10,6 @@ export MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ/
 
 mkdir -p "${expdir}"
 
-$pybin "${scriptdir}/trained_models.py" --eval_dir="${expdir}/" --model=slicq
+#$pybin "${scriptdir}/trained_models.py" --eval_dir="${expdir}/" --model=slicq
 $pybin "${scriptdir}/trained_models.py" --eval_dir="${expdir}/" --model=umx
 $pybin "${scriptdir}/trained_models.py" --eval_dir="${expdir}/" --model=xumx
