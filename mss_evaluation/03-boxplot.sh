2c4b75bdd67e0653e7f4a4defc145d27fdd85742
Sevag Hanssian <sevagh@protonmail.com>
2021-08-01 00:07:38 -0400

    Perform full boxplot eval

--- mss_evaluation/03-boxplot.sh
+++ mss_evaluation/03-boxplot.sh
@@ -4,7 +4,6 @@ pybin="/home/sevagh/venvs/thesis/bin/python3"
 expdir="exp-04-trained-models"
 scriptdir="./mss-oracle-experiments/oracle_eval"
 
-#export MUSDB_MAX_TRACKS=3
 export MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ
 
 # crunch and boxplot
