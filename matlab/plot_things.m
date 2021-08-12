5ef7a4501d2fd57d5add616039e088936048c32c
Sevag Hanssian <sevagh@protonmail.com>
2021-08-12 10:33:32 -0400

    More ch2 stuff

--- matlab/plot_things.m
+++ matlab/plot_things.m
@@ -16,3 +16,18 @@ grid on;
 xlabel('time (s)');
 ylabel('amplitude');
 title('Glockenspiel waveform');
+
+y = fft(sig);                               % Compute DFT of x
+m = abs(y);                               % Magnitude
+y(m<1e-6) = 0;
+p = unwrap(angle(y));                     % Phase
+
+f = (0:length(y)-1)*100/length(y);        % Frequency vector
+
+subplot(2,1,1)
+plot(f,m)
+title('Magnitude')
+
+subplot(2,1,2)
+plot(f,p*180/pi)
+title('Phase')
