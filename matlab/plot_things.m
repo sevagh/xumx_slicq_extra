f7d2ff3355b7f96e8a57b09cddaebfb48d0437e3
Sevag Hanssian <sevagh@protonmail.com>
2021-08-13 07:24:53 -0400

    Plot DFT of glockenspiel, explain DFT output

--- matlab/plot_things.m
+++ matlab/plot_things.m
@@ -17,17 +17,30 @@ xlabel('time (s)');
 ylabel('amplitude');
 title('Glockenspiel waveform');
 
-y = fft(sig);                               % Compute DFT of x
+N = siglen;
+y = fft(sig, N);                               % Compute DFT of x
 m = abs(y);                               % Magnitude
 y(m<1e-6) = 0;
 p = unwrap(angle(y));                     % Phase
 
-f = (0:length(y)-1)*100/length(y);        % Frequency vector
+df=fs/N; %frequency resolution
+sampleIndex = -N/2:N/2-1; %ordered index for FFT plot
+f=sampleIndex*df; %x-axis index converted to ordered frequencies
 
-subplot(2,1,1)
+subplot(3,1,1)
+plot(y, 'o')
+title('Complex DFT')
+xlabel('real');
+ylabel('imaginary');
+
+subplot(3,1,2)
 plot(f,m)
-title('Magnitude')
+title('Magnitude DFT');
+xlabel('frequency (Hz)');
+ylabel('|DFT|');
 
-subplot(2,1,2)
+subplot(3,1,3)
 plot(f,p*180/pi)
-title('Phase')
+title('Phase DFT')
+xlabel('frequency (Hz)');
+ylabel('\angle DFT');
