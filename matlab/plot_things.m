4f3846883b6065aab7be0f1758cd4888b063ccca
Sevag Hanssian <sevagh@protonmail.com>
2021-08-15 18:15:25 -0400

    Cleaned up ch2 stft section, fixed equations

--- matlab/plot_things.m
+++ matlab/plot_things.m
@@ -27,48 +27,53 @@ df=fs/N; %frequency resolution
 sampleIndex = -N/2:N/2-1; %ordered index for FFT plot
 f=sampleIndex*df; %x-axis index converted to ordered frequencies
 
-subplot(2,1,1)
-plot(f,m)
-title('Magnitude DFT');
-xlabel('frequency (Hz)');
-ylabel('|DFT|');
-
-subplot(2,1,2)
-plot(f,p*180/pi)
-title('Phase DFT')
-xlabel('frequency (Hz)');
-ylabel('\angle DFT');
-
-smallWin = 128;
-midWin = 2048;
-bigWin = 16384;
+% subplot(2,1,1)
+% plot(f,m)
+% title('Magnitude DFT');
+% xlabel('frequency (Hz)');
+% ylabel('|DFT|');
+% 
+% subplot(2,1,2)
+% plot(f,p*180/pi)
+% title('Phase DFT')
+% xlabel('frequency (Hz)');
+% ylabel('\angle DFT');
+% 
+% smallWin = 128;
+% midWin = 2048;
+% bigWin = 16384;
+% 
+% figure;
+% smallGauss=gausswin(smallWin);
+% spectrogram(sig,smallGauss,smallWin/2,smallWin*2,fs,"yaxis");
+% title("Glockenspiel, gausswin = 128");
+% 
+% figure;
+% midGauss=gausswin(midWin);
+% spectrogram(sig,midGauss,midWin/2,midWin*2,fs,"yaxis");
+% title("Glockenspiel, gausswin = 2048");
+% 
+% figure;
+% bigGauss=gausswin(bigWin);
+% spectrogram(sig,bigGauss,bigWin/2,bigWin*2,fs,"yaxis");
+% title("Glockenspiel, gausswin = 16384");
+% 
+% figure;
+% smallHamm=hamming(smallWin);
+% spectrogram(sig,smallHamm,smallWin/2,smallWin*2,fs,"yaxis");
+% title("Glockenspiel, hammwin = 128");
+% 
+% figure;
+% midHamm=hamming(midWin);
+% spectrogram(sig,midHamm,midWin/2,midWin*2,fs,"yaxis");
+% title("Glockenspiel, hammwin = 2048");
+% 
+% figure;
+% bigHamm=hamming(bigWin);
+% spectrogram(sig,bigHamm,bigWin/2,bigWin*2,fs,"yaxis");
+% title("Glockenspiel, hammwin = 16384");
 
 figure;
-smallGauss=gausswin(smallWin);
-spectrogram(sig,smallGauss,smallWin/2,smallWin*2,fs,"yaxis");
-title("Glockenspiel, gausswin = 128");
-
-figure;
-midGauss=gausswin(midWin);
-spectrogram(sig,midGauss,midWin/2,midWin*2,fs,"yaxis");
-title("Glockenspiel, gausswin = 2048");
-
-figure;
-bigGauss=gausswin(bigWin);
-spectrogram(sig,bigGauss,bigWin/2,bigWin*2,fs,"yaxis");
-title("Glockenspiel, gausswin = 16384");
-
-figure;
-smallHamm=hamming(smallWin);
-spectrogram(sig,smallHamm,smallWin/2,smallWin*2,fs,"yaxis");
-title("Glockenspiel, hammwin = 128");
-
-figure;
-midHamm=hamming(midWin);
-spectrogram(sig,midHamm,midWin/2,midWin*2,fs,"yaxis");
-title("Glockenspiel, hammwin = 2048");
-
-figure;
-bigHamm=hamming(bigWin);
-spectrogram(sig,bigHamm,bigWin/2,bigWin*2,fs,"yaxis");
-title("Glockenspiel, hammwin = 16384");
+plot(hamming(2048)); hold on; plot(gausswin(2048)); legend('Hamming window', 'Gaussian window');
+title('2048-point windows; Hamming vs. Gaussian');
+xlim([0 2048]);
