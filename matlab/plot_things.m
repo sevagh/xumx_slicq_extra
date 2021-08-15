373469c6c6e544a6bef6e6662fe4c2faade383c9
Sevag Hanssian <sevagh@protonmail.com>
2021-08-15 11:44:34 -0400

    STFT section feeling good

--- matlab/plot_things.m
+++ matlab/plot_things.m
@@ -17,7 +17,7 @@ xlabel('time (s)');
 ylabel('amplitude');
 title('Glockenspiel waveform');
 
-N = siglen;
+N = 2048;
 y = fft(sig, N);                               % Compute DFT of x
 m = abs(y);                               % Magnitude
 y(m<1e-6) = 0;
@@ -27,20 +27,48 @@ df=fs/N; %frequency resolution
 sampleIndex = -N/2:N/2-1; %ordered index for FFT plot
 f=sampleIndex*df; %x-axis index converted to ordered frequencies
 
-subplot(3,1,1)
-plot(y, 'o')
-title('Complex DFT')
-xlabel('real');
-ylabel('imaginary');
-
-subplot(3,1,2)
+subplot(2,1,1)
 plot(f,m)
 title('Magnitude DFT');
 xlabel('frequency (Hz)');
 ylabel('|DFT|');
 
-subplot(3,1,3)
+subplot(2,1,2)
 plot(f,p*180/pi)
 title('Phase DFT')
 xlabel('frequency (Hz)');
 ylabel('\angle DFT');
+
+smallWin = 128;
+midWin = 2048;
+bigWin = 16384;
+
+figure;
+smallGauss=gausswin(smallWin);
+spectrogram(sig,smallGauss,smallWin/2,smallWin*2,fs,"yaxis");
+title("Glockenspiel, gausswin = 128");
+
+figure;
+midGauss=gausswin(midWin);
+spectrogram(sig,midGauss,midWin/2,midWin*2,fs,"yaxis");
+title("Glockenspiel, gausswin = 2048");
+
+figure;
+bigGauss=gausswin(bigWin);
+spectrogram(sig,bigGauss,bigWin/2,bigWin*2,fs,"yaxis");
+title("Glockenspiel, gausswin = 16384");
+
+figure;
+smallHamm=hamming(smallWin);
+spectrogram(sig,smallHamm,smallWin/2,smallWin*2,fs,"yaxis");
+title("Glockenspiel, hammwin = 128");
+
+figure;
+midHamm=hamming(midWin);
+spectrogram(sig,midHamm,midWin/2,midWin*2,fs,"yaxis");
+title("Glockenspiel, hammwin = 2048");
+
+figure;
+bigHamm=hamming(bigWin);
+spectrogram(sig,bigHamm,bigWin/2,bigWin*2,fs,"yaxis");
+title("Glockenspiel, hammwin = 16384");
