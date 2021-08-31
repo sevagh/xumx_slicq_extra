4eccd5602bea88d3d30c9f8b15b050f003ba44dc
Sevag Hanssian <sevagh@protonmail.com>
2021-08-31 10:03:35 -0400

    Flesh out more of ch3

--- latex/scripts/slicenoslice.py
+++ latex/scripts/slicenoslice.py
@@ -9,21 +9,13 @@ K = 48    # high total bins
 fs = 44100 # musdb18-hq's cd sample rate
 
 log_scale = LogScale(Fmin, Fmax, K)
+test_signal = numpy.random.uniform(-1,1,fs*2) # 2s of random audio
 
-# test audio waveform - 2s of random samples in [-1.0, 1.0]
-test_signal = numpy.random.uniform(-1,1,fs*2)
+Ls = len(test_signal) # length of total signal
 
-# length of total signal
-Ls = len(test_signal)
-
-# nsgt takes the whole input signal length
-nsgt = NSGT(log_scale, fs, Ls)
-
-# sliced nsgt with inappropriate sllen, trlen = 2048, 512
-slicqt_warning = NSGT_sliced(log_scale, 2048, 512, fs)
-
-# finally, get the suggested sllen and trlen for a sample rate
-sllen, trlen = log_scale.suggested_sllen_trlen(fs)
+sllen, trlen = log_scale.suggested_sllen_trlen(fs) # get best suggested sllen/trlen
 print(f'suggested sllen, trlen: {sllen} {trlen}')
 
-slicqt_no_warning = NSGT_sliced(log_scale, sllen, trlen, fs)
+nsgt = NSGT(log_scale, fs, Ls) # nsgt operates on the entire signal
+slicqt_warning = NSGT_sliced(log_scale, 2048, 512, fs) # slicqt with bad sllen/trlen
+slicqt_no_warning = NSGT_sliced(log_scale, sllen, trlen, fs) # slicqt with good sllen/trlen
