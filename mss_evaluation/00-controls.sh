f3f3ae212d998015cfe5150507add31204bc71cc
Sevag Hanssian <sevagh@protonmail.com>
2021-09-01 12:57:47 -0400

    More Ch3, only slicqt + cdae left

--- mss_evaluation/00-controls.sh
+++ mss_evaluation/00-controls.sh
@@ -7,6 +7,7 @@ expdir="exp-00-controls"
 scriptdir="./mss-oracle-experiments/oracle_eval"
 
 export MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ
+export MUSDB_MAX_TRACKS=1
 
 mkdir -p "${expdir}"
 
@@ -19,7 +20,7 @@ params=(
 #"--oracle=mpi --control-window-sizes=1024"
 #"--oracle=irm1 --control-window-sizes=2048"
 #"--oracle=mpi --control-window-sizes=2048"
-#"--oracle=irm1 --control-window-sizes=4096"
+"--oracle=irm1 --control-window-sizes=4096"
 #"--oracle=mpi --control-window-sizes=4096"
 #"--oracle=irm1 --control-window-sizes=8192"
 #"--oracle=mpi --control-window-sizes=8192"
@@ -27,9 +28,11 @@ params=(
 #"--oracle=mpi --control-window-sizes=16384"
 #"--oracle=irm1 --fixed-slicqt-param=bark,262,32.9"
 #"--oracle=mpi --fixed-slicqt-param=bark,262,32.9"
-"--oracle=irm1 --fixed-slicqt-param=cqlog,142,129.7"
-"--oracle=mpi --fixed-slicqt-param=cqlog,142,129.7"
+#"--oracle=irm1 --fixed-slicqt-param=cqlog,142,129.7"
+#"--oracle=mpi --fixed-slicqt-param=cqlog,142,129.7"
 )
 
 # limit to 2
-parallel --jobs=1 --colsep=' ' --ungroup $pybin "${scriptdir}/search_best_nsgt.py" --fixed-slicqt --eval-dir="${expdir}" --cuda-device={#} {} ::: "${params[@]}" 
+#parallel --jobs=1 --colsep=' ' --ungroup $pybin "${scriptdir}/search_best_nsgt.py" --fixed-slicqt --eval-dir="${expdir}" --cuda-device={#} {} ::: "${params[@]}" 
+
+time $pybin -m kernprof -l -v "${scriptdir}/search_best_nsgt.py" --fixed-slicqt --eval-dir="${expdir}" --cuda-device=0 --oracle=irm1 --control-window-sizes=4096 --control
