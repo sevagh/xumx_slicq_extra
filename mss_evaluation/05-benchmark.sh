f806e03d91cde7a6fe166f7e899546c5595beb32
Sevag Hanssian <sevagh@protonmail.com>
2021-09-04 18:17:14 -0400

    BSS benchmarks seem ok

--- mss_evaluation/05-benchmark.sh
+++ mss_evaluation/05-benchmark.sh
@@ -8,5 +8,6 @@ scriptdir="./mss-oracle-experiments/oracle_eval"
 export MUSDB_PATH=/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ
 #export MUSDB_MAX_TRACKS=1
 
-$pybin "${scriptdir}/benchmark_cupy_eval.py" --cuda-device=0 --bench-iter=10 --split=valid
-$pybin "${scriptdir}/benchmark_cupy_eval.py" --cuda-device=0 --bench-iter=10 --split=valid --disable-cupy
+#$pybin "${scriptdir}/benchmark_cupy_eval.py" --cuda-device=0 --bench-iter=10 --split=valid
+#$pybin "${scriptdir}/benchmark_cupy_eval.py" --cuda-device=0 --bench-iter=10 --split=valid --disable-cupy
+$pybin "${scriptdir}/benchmark_cupy_eval.py" --cuda-device=1 --bench-iter=10 --split=valid
