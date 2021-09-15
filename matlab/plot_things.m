f3e3508cd26ddff43e391c8c91de856b823ec763
Sevag Hanssian <sevagh@protonmail.com>
2021-09-15 08:43:17 -0400

    Redo Python NSGT/sliCQT plots and fix some MATLAB plot fonts

--- matlab/plot_things.m
+++ matlab/plot_things.m
@@ -14,14 +14,14 @@ duration = siglen/fs;
 dT = 1/fs;
 t = (0.0:dT:duration-dT)';
 
-figure;
-plot(t, sig);
-grid on;
-xlabel('time (s)');
-ylabel('amplitude');
-title('Glockenspiel waveform','FontWeight','Normal');
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,"../latex/images-gspi/glock_waveform.png","Resolution",dpiVar);
+% figure;
+% plot(t, sig);
+% grid on;
+% xlabel('time (s)');
+% ylabel('amplitude');
+% title('Glockenspiel waveform','FontWeight','Normal');
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,"../latex/images-gspi/glock_waveform.png","Resolution",dpiVar);
 
 N = 2048;
 y = fft(sig, N);                               % Compute DFT of x
@@ -37,16 +37,20 @@ figure;
 subplot(2,1,1)
 plot(f,m)
 title(sprintf('Magnitude DFT, %d points', N),'FontWeight','Normal');
+grid on;
 xlabel('frequency (Hz)');
 ylabel('|DFT|');
+set(gca, 'FontSize', 21); % smaller font on DFTs
 
 subplot(2,1,2)
 plot(f,p*180/pi)
 title(sprintf('Phase DFT, %d points', N),'FontWeight','Normal');
+grid on;
 xlabel('frequency (Hz)');
 ylabel('\angle DFT');
 
 set(gcf, 'Position', gcfPosition);
+set(gca, 'FontSize', 21); % smaller font on DFTs
 exportgraphics(gcf,sprintf("../latex/images-gspi/glock_dft_%d.png", N),"Resolution",dpiVar);
 
 N = siglen;
@@ -63,118 +67,123 @@ figure;
 subplot(2,1,1)
 plot(f,m)
 title(sprintf('Magnitude DFT, %d points', N),'FontWeight','Normal');
+grid on;
 xlabel('frequency (Hz)');
 ylabel('|DFT|');
+set(gca, 'FontSize', 21); % smaller font on DFTs
 
 subplot(2,1,2)
 plot(f,p*180/pi)
 title(sprintf('Phase DFT, %d points', N),'FontWeight','Normal');
+grid on;
 xlabel('frequency (Hz)');
 ylabel('\angle DFT');
 
 set(gcf, 'Position', gcfPosition);
+set(gca, 'FontSize', 21); % smaller font on DFTs
 exportgraphics(gcf,sprintf("../latex/images-gspi/glock_dft_%d.png", N),"Resolution",dpiVar);
 
-smallWin = 128;
-midWin = 2048;
-bigWin = 16384;
-
-figure;
-smallGauss=gausswin(smallWin);
-spectrogram(sig,smallGauss,smallWin/2,smallWin*2,fs,"yaxis");
-title("Glockenspiel, gausswin = 128",'FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,sprintf("../latex/images-gspi/glock_gauss_%d.png", smallWin),"Resolution",dpiVar);
-
-figure;
-midGauss=gausswin(midWin);
-spectrogram(sig,midGauss,midWin/2,midWin*2,fs,"yaxis");
-title("Glockenspiel, gausswin = 2048",'FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,sprintf("../latex/images-gspi/glock_gauss_%d.png", midWin),"Resolution",dpiVar);
-
-figure;
-bigGauss=gausswin(bigWin);
-spectrogram(sig,bigGauss,bigWin/2,bigWin*2,fs,"yaxis");
-title("Glockenspiel, gausswin = 16384",'FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,sprintf("../latex/images-gspi/glock_gauss_%d.png", bigWin),"Resolution",dpiVar);
-
-figure;
-smallHamm=hamming(smallWin);
-spectrogram(sig,smallHamm,smallWin/2,smallWin*2,fs,"yaxis");
-title("Glockenspiel, hammwin = 128",'FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,sprintf("../latex/images-gspi/glock_hamm_%d.png", smallWin),"Resolution",dpiVar);
-
-figure;
-midHamm=hamming(midWin);
-spectrogram(sig,midHamm,midWin/2,midWin*2,fs,"yaxis");
-title("Glockenspiel, hammwin = 2048",'FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,sprintf("../latex/images-gspi/glock_hamm_%d.png", midWin),"Resolution",dpiVar);
-
-figure;
-bigHamm=hamming(bigWin);
-spectrogram(sig,bigHamm,bigWin/2,bigWin*2,fs,"yaxis");
-title("Glockenspiel, hammwin = 16384",'FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,sprintf("../latex/images-gspi/glock_hamm_%d.png", bigWin),"Resolution",dpiVar);
+% smallWin = 128;
+% midWin = 2048;
+% bigWin = 16384;
+% 
+% figure;
+% smallGauss=gausswin(smallWin);
+% spectrogram(sig,smallGauss,smallWin/2,smallWin*2,fs,"yaxis");
+% title("Glockenspiel, gausswin = 128",'FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,sprintf("../latex/images-gspi/glock_gauss_%d.png", smallWin),"Resolution",dpiVar);
+% 
+% figure;
+% midGauss=gausswin(midWin);
+% spectrogram(sig,midGauss,midWin/2,midWin*2,fs,"yaxis");
+% title("Glockenspiel, gausswin = 2048",'FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,sprintf("../latex/images-gspi/glock_gauss_%d.png", midWin),"Resolution",dpiVar);
+% 
+% figure;
+% bigGauss=gausswin(bigWin);
+% spectrogram(sig,bigGauss,bigWin/2,bigWin*2,fs,"yaxis");
+% title("Glockenspiel, gausswin = 16384",'FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,sprintf("../latex/images-gspi/glock_gauss_%d.png", bigWin),"Resolution",dpiVar);
+% 
+% figure;
+% smallHamm=hamming(smallWin);
+% spectrogram(sig,smallHamm,smallWin/2,smallWin*2,fs,"yaxis");
+% title("Glockenspiel, hammwin = 128",'FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,sprintf("../latex/images-gspi/glock_hamm_%d.png", smallWin),"Resolution",dpiVar);
+% 
+% figure;
+% midHamm=hamming(midWin);
+% spectrogram(sig,midHamm,midWin/2,midWin*2,fs,"yaxis");
+% title("Glockenspiel, hammwin = 2048",'FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,sprintf("../latex/images-gspi/glock_hamm_%d.png", midWin),"Resolution",dpiVar);
+% 
+% figure;
+% bigHamm=hamming(bigWin);
+% spectrogram(sig,bigHamm,bigWin/2,bigWin*2,fs,"yaxis");
+% title("Glockenspiel, hammwin = 16384",'FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,sprintf("../latex/images-gspi/glock_hamm_%d.png", bigWin),"Resolution",dpiVar);
 
 figure;
 plot(hamming(2048)); hold on; plot(gausswin(2048)); legend('Hamming window', 'Gaussian window');
 title('2048-point windows; Hamming vs. Gaussian','FontWeight','Normal');
-xlim([0 2048]);
+xlim([-64 2048+64]);
+grid on;
 
 set(gcf, 'Position', gcfPosition);
 exportgraphics(gcf,"../latex/images-tftheory/hamming_vs_gauss.png","Resolution",dpiVar);
 
-figure;
-spectrogram(sig,4096,1024,4096,fs,'yaxis');
-title('Magnitude STFT, window=4096','FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,"../latex/images-gspi/glock_stft_4096.png","Resolution",dpiVar);
-
-figure;
-spectrogram(sig,1024,256,1024,fs,'yaxis');
-title('Magnitude STFT, window=1024','FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,"../latex/images-gspi/glock_stft_1024.png","Resolution",dpiVar);
-
-figure;
-spectrogram(sig,256,64,256,fs,'yaxis');
-title('Magnitude STFT, window=256','FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,"../latex/images-gspi/glock_stft_256.png","Resolution",dpiVar);
-
-figure;
-cqt(sig,'SamplingFrequency',fs,'BinsPerOctave',12);
-title('Magnitude CQT/CQ-NSGT, 12 bins-per-octave','FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,"../latex/images-gspi/glock_cqt12.png","Resolution",dpiVar);
-
-figure;
-cqt(sig,'SamplingFrequency',fs,'BinsPerOctave',24);
-title('Magnitude CQT/CQ-NSGT, 24 bins-per-octave','FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,"../latex/images-gspi/glock_cqt24.png","Resolution",dpiVar);
-
-figure;
-cqt(sig,'SamplingFrequency',fs,'BinsPerOctave',48);
-title('Magnitude CQT/CQ-NSGT, 48 bins-per-octave','FontWeight','Normal');
-
-set(gcf, 'Position', gcfPosition);
-exportgraphics(gcf,"../latex/images-gspi/glock_cqt48.png","Resolution",dpiVar);
+% figure;
+% spectrogram(sig,4096,1024,4096,fs,'yaxis');
+% title('Magnitude STFT, window=4096','FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,"../latex/images-gspi/glock_stft_4096.png","Resolution",dpiVar);
+% 
+% figure;
+% spectrogram(sig,1024,256,1024,fs,'yaxis');
+% title('Magnitude STFT, window=1024','FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,"../latex/images-gspi/glock_stft_1024.png","Resolution",dpiVar);
+% 
+% figure;
+% spectrogram(sig,256,64,256,fs,'yaxis');
+% title('Magnitude STFT, window=256','FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,"../latex/images-gspi/glock_stft_256.png","Resolution",dpiVar);
+% 
+% figure;
+% cqt(sig,'SamplingFrequency',fs,'BinsPerOctave',12);
+% title('Magnitude CQT/CQ-NSGT, 12 bins-per-octave','FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,"../latex/images-gspi/glock_cqt12.png","Resolution",dpiVar);
+% 
+% figure;
+% cqt(sig,'SamplingFrequency',fs,'BinsPerOctave',24);
+% title('Magnitude CQT/CQ-NSGT, 24 bins-per-octave','FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,"../latex/images-gspi/glock_cqt24.png","Resolution",dpiVar);
+% 
+% figure;
+% cqt(sig,'SamplingFrequency',fs,'BinsPerOctave',48);
+% title('Magnitude CQT/CQ-NSGT, 48 bins-per-octave','FontWeight','Normal');
+% 
+% set(gcf, 'Position', gcfPosition);
+% exportgraphics(gcf,"../latex/images-gspi/glock_cqt48.png","Resolution",dpiVar);
 
 close all;
--- nsgt_spec_plotter/gen_plots.sh
+++ nsgt_spec_plotter/gen_plots.sh
@@ -8,22 +8,34 @@ gspipath="../matlab/vendor/ltfat/signals/gspi.wav"
 
 # psychoacoustic scales
 
-#${pybin} ${scriptdir}/spectrogram.py --scale=mel --fmin=20 --fmax=22050 --bins=500 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_mel_500.png"
-#
-#${pybin} ${scriptdir}/spectrogram.py --scale=mel --fmin=20 --fmax=22050 --bins=100 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_mel_100.png"
-#
-#${pybin} ${scriptdir}/spectrogram.py --scale=bark --fmin=20 --fmax=22050 --bins=100 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_bark_100.png"
-#
-#${pybin} ${scriptdir}/spectrogram.py --scale=bark --fmin=20 --fmax=22050 --bins=500 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_bark_500.png"
+${pybin} ${scriptdir}/spectrogram.py --scale=mel --fmin=20 --fmax=22050 --bins=500 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_mel_500.png" --nonsliced
+
+${pybin} ${scriptdir}/spectrogram.py --scale=mel --fmin=20 --fmax=22050 --bins=100 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_mel_100.png" --nonsliced
+
+${pybin} ${scriptdir}/spectrogram.py --scale=bark --fmin=20 --fmax=22050 --bins=100 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_bark_100.png" --nonsliced
+
+${pybin} ${scriptdir}/spectrogram.py --scale=bark --fmin=20 --fmax=22050 --bins=500 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_bark_500.png" --nonsliced
 
 # constant-q and variable-q
 
-#${pybin} ${scriptdir}/spectrogram.py --scale=cqlog --fmin=20 --fmax=22050 --bins=100 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_cqlog_100.png"
-#
-#${pybin} ${scriptdir}/spectrogram.py --scale=cqlog --fmin=20 --fmax=22050 --bins=500 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_cqlog_500.png"
-#
-#${pybin} ${scriptdir}/spectrogram.py --scale=vqlog --gamma=15 --fmin=20 --fmax=22050 --bins=100 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_vqlog_100.png"
-#
-#${pybin} ${scriptdir}/spectrogram.py --scale=vqlog --gamma=15 --fmin=20 --fmax=22050 --bins=500 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_vqlog_500.png"
+${pybin} ${scriptdir}/spectrogram.py --scale=cqlog --fmin=20 --fmax=22050 --bins=100 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_cqlog_100.png" --nonsliced
 
+${pybin} ${scriptdir}/spectrogram.py --scale=cqlog --fmin=20 --fmax=22050 --bins=500 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_cqlog_500.png" --nonsliced
 
+${pybin} ${scriptdir}/spectrogram.py --scale=vqlog --gamma=15 --fmin=20 --fmax=22050 --bins=100 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_vqlog_100.png" --nonsliced
+
+${pybin} ${scriptdir}/spectrogram.py --scale=vqlog --gamma=15 --fmin=20 --fmax=22050 --bins=500 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_vqlog_500.png" --nonsliced
+
+# misc, overlaps, etc.
+
+${pybin} ${scriptdir}/spectrogram.py --scale=mel --fmin=20 --fmax=22050 --bins=96 --plot --cmap inferno "${gspipath}" --fontsize=34 --nonsliced --output="../latex/images-gspi/gspi_nsgt_mel_nooverlap.png"
+
+${pybin} ${scriptdir}/spectrogram.py --scale=mel --fmin=20 --fmax=22050 --bins=96 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_nsgt_mel_perfect_slice.png"
+
+${pybin} ${scriptdir}/spectrogram.py --scale=bark --fmin=20 --fmax=22050 --bins=84 --plot --cmap inferno "${gspipath}" --fontsize=34 --flatten --output="../latex/images-gspi/gspi_overlap_flatten.png"
+
+${pybin} ${scriptdir}/spectrogram.py --scale=bark --fmin=20 --fmax=22050 --bins=84 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/gspi_overlap_proper.png"
+
+${pybin} ${scriptdir}/spectrogram.py --scale=cqlog --fmin=129.7 --fmax=22050 --bins=142 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/slicqt_bad.png"
+
+${pybin} ${scriptdir}/spectrogram.py --scale=bark --fmin=32.9 --fmax=22050 --bins=262 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/images-gspi/slicqt_good.png"
