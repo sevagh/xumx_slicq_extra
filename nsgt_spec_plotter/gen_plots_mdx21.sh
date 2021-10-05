61aff7aadc6e0a879e32ef21ce7ba5ae449dbe90
Sevag Hanssian <sevagh@protonmail.com>
2021-10-05 07:35:21 -0400

    Add poster files

--- nsgt_spec_plotter/gen_plots_mdx21.sh
+++ nsgt_spec_plotter/gen_plots_mdx21.sh
@@ -9,3 +9,6 @@ gspipath="../matlab/vendor/ltfat/signals/gspi.wav"
 ${pybin} ${scriptdir}/spectrogram.py --scale=Bark --fmin=32.9 --fmax=22050 --bins=262 --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/mdx-submissions21/slicq.png"
 
 ${pybin} stft_spectrogram.py --plot --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/mdx-submissions21/stft.png"
+${pybin} stft_spectrogram.py --plot --window=1024 --overlap=256 --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/mdx-submissions21/stft_small.png"
+
+${pybin} stft_spectrogram.py --plot --window=16384 --overlap=4096 --cmap inferno "${gspipath}" --fontsize=34 --output="../latex/mdx-submissions21/stft_big.png"
