e30f7f1289d6aaabc2f372778fad3730fe0fe0fc
Sevag Hanssian <sevagh@protonmail.com>
2022-04-08 08:16:15 -0400

    Add original UMX release 1.0.0

--- mss_evaluation/03-boxplot.sh
+++ mss_evaluation/03-boxplot.sh
@@ -8,7 +8,6 @@ scriptdir="./mss-oracle-experiments/oracle_eval"
 #$pybin "${scriptdir}/boxplot.py" --colors-legend=control ./exp-00-controls/data.pandas ./exp-00-controls/boxplot.pdf
 
 # pretrained model + 2 controls - crunch into pandas + boxplot
-$pybin "${scriptdir}/aggregate.py" --out=./exp-00-controls-doctored/data.pandas ./exp-00-controls-doctored/*
-$pybin "${scriptdir}/boxplot.py" ./exp-00-controls-doctored/data.pandas ./exp-00-controls-doctored/boxplot.pdf --colors-legend=control
-$pybin "${scriptdir}/boxplot.py" --print-median-only ./exp-00-controls-doctored/data.pandas ./exp-00-controls-doctored/boxplot.pdf
-#$pybin "${scriptdir}/boxplot.py" --print-median-only --colors-legend=pretrained ./exp-00-controls/data.pandas ./exp-00-controls/boxplot.pdf
+$pybin "${scriptdir}/aggregate.py" --out=./exp-04-trained-models/data.pandas ./exp-04-trained-models/*
+$pybin "${scriptdir}/boxplot.py" --print-median-only ./exp-04-trained-models/data.pandas ./exp-04-trained-models/boxplot.pdf
+$pybin "${scriptdir}/boxplot.py" ./exp-04-trained-models/data.pandas ./exp-04-trained-models/boxplot.pdf
--- mss_evaluation/04-umx-models.sh
+++ mss_evaluation/04-umx-models.sh
@@ -3,16 +3,21 @@
 set -eoxu pipefail
 
 pybin="/home/sevagh/venvs/thesis/bin/python3"
-expdir="exp-04-trained-models-test"
+expdir="exp-04-trained-models"
 scriptdir="./mss-oracle-experiments/oracle_eval"
+umxorigdir="./vendor/open-unmix-pytorch-1.0.0"
+umxmodeldir="./pretrained_models/umx"
+umxexpdir="exp-04-trained-models-umx-orig"
 
 export MUSDB_PATH=/run/media/sevagh/windows-games/MDX-datasets/MUSDB18-HQ/
-export CUDA_VISIBLE_DEVICES=0
+#export CUDA_VISIBLE_DEVICES=0
 #export MUSDB_MAX_TRACKS=10
 
 mkdir -p "${expdir}"
 
+#$pybin "${scriptdir}/trained_models.py" --eval_dir="${expdir}/" --model=umx
 #$pybin "${scriptdir}/trained_models.py" --eval_dir="${expdir}/" --model=xumx
-$pybin "${scriptdir}/trained_models.py" --eval_dir="${expdir}/" --model=umx --track-name='Georgia Wonder - Siren'
 #$pybin "${scriptdir}/trained_models.py" --eval_dir="${expdir}/" --model=slicq-wslicq
 #$pybin "${scriptdir}/trained_models.py" --eval_dir="${expdir}/" --model=slicq-wstft
+
+$pybin "${umxorigdir}/eval.py" --root="${MUSDB_PATH}" --is-wav --no-cuda --model="${umxmodeldir}" --evaldir="${umxexpdir}/"
